42288f414f7dbb83b04abbdc0bed5d928d24c0fb
Danon4ik <fadescode@gmail.com>
2021-01-20 18:30:37 +0500

    Removed routes

--- api/index.js
+++ api/index.js
@@ -1,20 +1,7 @@
 const express = require('express')
-
-// Create express instance
 const app = express()
 
-// Require API routes
-const users = require('./routes/users')
-const test = require('./routes/test')
-
-// Import API Routes
-app.use(users)
-app.use(test)
-
-// Export express app
 module.exports = app
-
-// Start standalone server if directly running
 if (require.main === module) {
   const port = process.env.PORT || 3001
   app.listen(port, () => {
