4dcef20263f7c11b5b7128f08fb5beb95583d39f
Danon <fadescode@gmail.com>
2021-02-23 17:09:03 +0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,8 +17,11 @@ module.exports = {
   rules: {
     'vue/singleline-html-element-content-newline': 'off',
     'vue/max-attributes-per-line': 'off',
+    'vue/require-prop-types': 'off',
     'space-before-function-paren': 'off',
+    'object-shorthand': 'off',
+    'arrow-parens': 'off',
     'no-console': 'off',
-    curly: [2, 'multi']
+    curly: 'off'
   }
 }
