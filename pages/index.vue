fcdcc07e3a55063c87ceffb5f1c91a404ff12d02
Danon <fadescode@gmail.com>
2021-02-23 17:18:03 +0500

    Update pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,35 @@
 <template>
   <div class="container">
+    <section>
+      <h1 class="text-center">Все категории</h1>
+      <h2 class="text-center">Здесь вы можете найти все, что душе угодно</h2>
+      <div v-for="(cat, key) in categories" :key="key" class="row justify-content-center">
+        <CategoryCard :title="cat.title" :to="cat.to" />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  data() {
+    return {
+      categories: [
+        {
+          title: 'Категория 1',
+          to: '/categories/cat1'
+        },
+        {
+          title: 'Категория 2',
+          to: '/categories/cat2'
+        },
+        {
+          title: 'Категория 3',
+          to: '/categories/cat3'
+        }
+      ]
+    }
+  }
 }
 </script>
