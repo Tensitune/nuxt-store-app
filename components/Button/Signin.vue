426a37d637c7ee0b5b3781baa2a7d139218f9a69
Danon <fadescode@gmail.com>
2021-03-23 00:23:11 +0500

    Added sign in and sign up buttons with dialogs

---
+++ components/Button/Signin.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-dialog v-model="dialog" max-width="600px">
+    <template #activator="{ on, attrs }">
+      <v-btn color="primary" outlined rounded v-bind="attrs" v-on="on">
+        Войти
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Войти</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field v-model="signinData.username" label="Логин" required />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                v-model="signinData.password"
+                :append-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                :type="passwordVisible ? '' : 'password'"
+                label="Пароль"
+                required
+                @click:append="passwordVisible = !passwordVisible"
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+        <h3 v-if="error" class="red--text">{{ error }}</h3>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="blue darken-1" text @click="dialog = false">
+          Закрыть
+        </v-btn>
+        <v-btn color="blue darken-1" text @click="signIn">
+          Войти
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    error: '',
+    passwordVisible: false,
+    signinData: {
+      username: '',
+      password: ''
+    }
+  }),
+  methods: {
+    signIn() {
+      if (!(this.signinData.username && this.signinData.password)) {
+        this.error = 'Заполните все поля'
+        return
+      }
+
+      this.$axios.post('/signin', this.signinData).then(res => {
+        if (res.data.status === 'error') {
+          this.error = res.data.error
+          return
+        }
+
+        window.location.reload()
+      }).catch(err => console.log(err))
+    }
+  }
+}
+</script>
---
+++ components/Button/Signup.vue
@@ -0,0 +1,126 @@
+<template>
+  <v-dialog ref="form" v-model="dialog" max-width="600px">
+    <template #activator="{ on, attrs }">
+      <v-btn color="primary" rounded v-bind="attrs" v-on="on">
+        Регистрация
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="headline">Регистрация</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field v-model="signupData.username" label="Имя пользователя*" :rules="requiredRules" required />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                v-model="signupData.password"
+                :append-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                :rules="requiredRules"
+                :type="passwordVisible ? '' : 'password'"
+                label="Пароль*"
+                required
+                @click:append="passwordVisible = !passwordVisible"
+              />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="passwordConfirm" label="Подтвержение пароля*" :rules="requiredRules" :type="passwordVisible ? '' : 'password'" required />
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field v-model="signupData.firstname" label="Имя*" :rules="requiredRules" required />
+            </v-col>
+            <v-col cols="12" sm="6" md="6">
+              <v-text-field v-model="signupData.lastname" label="Фамилия*" :rules="requiredRules" required />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="signupData.phone" label="Телефон" />
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="signupData.address" label="Адрес" />
+            </v-col>
+          </v-row>
+        </v-container>
+        <h3 v-if="error" class="red--text">{{ error }}</h3>
+        <small>* - указывает на обязательное поле</small>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="blue darken-1" text @click="dialog = false; loading = false;">
+          Закрыть
+        </v-btn>
+        <v-btn color="blue darken-1" type="submit" :loading="loading" text @click="signUp">
+          Регистрация
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    loading: false,
+    passwordVisible: false,
+    error: '',
+    signupData: {
+      username: '',
+      password: '',
+      firstname: '',
+      lastname: '',
+      phone: '',
+      address: ''
+    },
+    passwordConfirm: '',
+    requiredRules: [
+      v => !!v || 'Это поле обязательно для заполнения'
+    ]
+  }),
+  methods: {
+    signUp() {
+      this.loading = true
+
+      if (!(this.signupData.username && this.signupData.password && this.signupData.firstname && this.signupData.lastname)) {
+        this.error = 'Заполните обязательные поля'
+        this.loading = false
+        return
+      }
+
+      if (this.signupData.password !== this.passwordConfirm) {
+        this.error = 'Пароли не совпадают'
+        this.loading = false
+        return
+      }
+
+      this.$axios.post('/signup', this.signupData).then(res => {
+        this.error = ''
+        this.loading = false
+
+        if (res.data.status === 'error') {
+          this.error = res.data.error
+          return
+        }
+
+        this.signupData.username = ''
+        this.signupData.password = ''
+        this.signupData.firstname = ''
+        this.signupData.lastname = ''
+        this.signupData.phone = ''
+        this.signupData.address = ''
+
+        this.passwordConfirm = ''
+        this.passwordVisible = false
+
+        this.dialog = false
+      }).catch(err => {
+        console.log(err)
+        this.error = 'Что-то пошло не так'
+        this.loading = false
+      })
+    }
+  }
+}
+</script>
