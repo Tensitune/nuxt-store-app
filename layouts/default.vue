8338ba018b8d13a61c49215fa3ae9cb0cd8d3787
Danon <fadescode@gmail.com>
2021-03-25 22:06:07 +0500

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,8 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
+    <v-navigation-drawer v-if="!showDrawer" v-model="drawer" :clipped="clipped" fixed app>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items" :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -19,17 +14,15 @@
     </v-navigation-drawer>
 
     <v-app-bar :clipped-left="clipped" dense fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-app-bar-nav-icon v-if="!showDrawer" @click.stop="drawer = !drawer" />
       <v-toolbar-title class="mr-1" v-text="title" />
-      <v-btn v-for="(item, i) in items" :key="i" :to="item.to" class="ml-1 mr-1" router text :href="item.url" target="_blank">
-        <v-icon class="nav-icon">{{ item.icon }}</v-icon>
-        <h4 v-if="showNavTitle">{{ item.title }}</h4>
-      </v-btn>
 
-      <v-btn v-if="user" to="/cart" class="ml-1 mr-1" router text href="/cart" target="_blank">
-        <v-icon class="nav-icon">mdi-cart-outline</v-icon>
-        <h4 v-if="showNavTitle">Корзина</h4>
-      </v-btn>
+      <template v-if="showDrawer">
+        <v-btn v-for="(item, i) in items" :key="i" :to="item.to" class="mx-1" router text>
+          <v-icon class="nav-icon">{{ item.icon }}</v-icon>
+          <h4>{{ item.title }}</h4>
+        </v-btn>
+      </template>
 
       <v-spacer />
 
@@ -37,8 +30,29 @@
         <ButtonSignin />
         <ButtonSignup />
       </div>
-      <div v-else>
-        {{ user.username }}
+      <div v-else class="text-center">
+        <v-menu offset-y transition="slide-y-transition">
+          <template #activator="{ on, attrs }">
+            <v-btn color="secondary" text v-bind="attrs" v-on="on">
+              <v-icon>mdi-account</v-icon>
+              {{ user.username }}
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-if="user.admin" link router to="/admin">
+              <v-list-item-title class="d-flex justify-start align-center">
+                <v-icon>mdi-shield-account</v-icon>
+                Админ-панель
+              </v-list-item-title>
+            </v-list-item>
+            <v-list-item link @click="signOut">
+              <v-list-item-title class="d-flex justify-start align-center">
+                <v-icon>mdi-logout</v-icon>
+                Выйти
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
       </div>
     </v-app-bar>
 
@@ -49,9 +63,7 @@
         <v-card flat tile class="indigo lighten-1 white--text text-center" width="100%">
           <v-card-text>
             <v-btn v-for="item in footerIcons" :key="item.icon" class="mx-4 white--text" router :href="item.uri" target="_blank" icon>
-              <v-icon size="24px">
-                {{ item.icon }}
-              </v-icon>
+              <v-icon size="24px">{{ item.icon }}</v-icon>
             </v-btn>
           </v-card-text>
 
@@ -97,6 +109,11 @@ export default {
         icon: 'mdi-email-outline',
         title: 'Обратная связь',
         to: '/feedback'
+      },
+      {
+        icon: 'mdi-cart-outline',
+        title: 'Корзина',
+        to: '/cart'
       }
     ],
     footerIcons: [
@@ -110,7 +127,7 @@ export default {
     user() {
       return this.$store.getters['user/data']
     },
-    showNavTitle() {
+    showDrawer() {
       const disabledSizes = ['xs', 'sm', 'md']
       if (disabledSizes.includes(this.$vuetify.breakpoint.name)) return false
       return true
@@ -119,8 +136,15 @@ export default {
   mounted() {
     this.$axios.$get('/profile').then(res => {
       if (res.status === 'error') return
-      this.$store.commit('user/set', res.user)
+      this.$store.commit('user/set', res.data)
     })
+  },
+  methods: {
+    signOut() {
+      this.$axios.get('/signout').then(() => {
+        window.location.reload()
+      })
+    }
   }
 }
 </script>
