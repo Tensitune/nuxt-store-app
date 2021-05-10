ffbb67fbce7344643126d136c0ae5887df7b2b0a
Danon <fadescode@gmail.com>
2021-05-10 15:35:18 +0500

    Added pagination component

---
+++ components/Pagination.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <slot />
+
+    <div v-if="count > limit" class="my-3 text-center">
+      <v-pagination v-model="page" :length="Math.round(count / limit)" :total-visible="7" color="secondary" circle />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      required: true
+    },
+    limit: {
+      type: Number,
+      required: true
+    }
+  },
+  data: () => ({
+    page: 1
+  }),
+  watch: {
+    page: function(newPage) {
+      this.$emit('onPageChange', newPage)
+    }
+  }
+}
+</script>
