1f5e3e044700ea508a329a46aebde3c21febb252
Danon <fadescode@gmail.com>
2021-05-14 16:11:43 +0500

    Fixed admin middleware

--- api/middleware/AdminMiddleware.js
+++ api/middleware/AdminMiddleware.js
@@ -1,10 +1,10 @@
-const { find } = require('../db')
+const { findOne } = require('../db')
 
 module.exports = async (req, res, next) => {
-  if (!req.session.userid) return res.redirect('/')
+  if (!req.session.userid) return res.json({ status: 'error', error: 'Вы не вошли в свой профиль' })
 
-  const user = await find('users', { id: req.session.userid })
-  if (!(user && user.admin)) return res.redirect('/')
+  const user = await findOne('users', { id: req.session.userid })
+  if (!(user && user.admin)) return res.json({ status: 'error', error: 'Вы не являетесь администратором' })
 
   next()
 }
